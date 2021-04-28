ce5e344c68259e9ca0322b70c11bb31da2173b74
aplkaev <hernezie@gmail.com>
2021-04-28 11:01:05 +0300

    remove settings rest_freamwork

--- bash/bash/settings.py
+++ bash/bash/settings.py
@@ -26,8 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'rest_framework.authtoken'
+    'main'
 ]
 
 MIDDLEWARE = [
